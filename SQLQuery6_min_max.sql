bad09302908c493b72c8b2a72eed92d0b16d9e71
pranav7491 <sathepranav77@gmail.com>
2019-02-10 04:32:30 -0600

    Update2

--- SQLQuery6_min_max.sql
+++ SQLQuery6_min_max.sql
@@ -14,10 +14,16 @@ select
 [claimstatus],
 min([claimpaidamount]) as m_cpa,
 sum([claimpaidamount]) as s_cpa,
-count(ClaimCause) as c_cc
+count(ClaimCause) as c_cc,
+((min([claimpaidamount]))/sum([claimpaidamount])) as pcntg
+
 from
 [Chapter 4 - Insurance].[dbo].[MemberClaims]
+where
+[claimstatus] = 'Paid'
 group by
 [ClaimCauseCategory],
 [ClaimCause],
 [claimstatus]
+order by
+1
